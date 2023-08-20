c7aaf3d64bcd4e72412718f4f07725f405504288
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-08-20 23:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let timer = document.getElementById('timer');
 let newDate =new Date();
 let date = new Date().toLocaleString();
 
-let fullDate = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}, `
+// let fullDate = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}, `
 // let year = newDate.getFullYear()
 timer.innerText = date
